c39b5b0df68f76d736b385dd8231c8e823f6403a
romainsacchi <romain@Romains-iMac.psi.ch>
2025-09-10 16:46:35 +0200

    version increment

--- edges/__init__.py
+++ edges/__init__.py
@@ -5,7 +5,7 @@ applying characterization factors conditioned by the context of exchanges.
 
 __all__ = ("EdgeLCIA", "CostLCIA", "get_available_methods")
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 from .logging_config import setup_package_logging
 from .edgelcia import EdgeLCIA
