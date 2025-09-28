49703038b822155f1f5a2a8eeb5c4ecb0178d614
romainsacchi <r_s@me.com>
2025-09-28 18:17:22 +0200

    Add test

--- tests/test_AWARE.py
+++ tests/test_AWARE.py
@@ -21,12 +21,13 @@ system_model = "cutoff"
 
 bw2data.projects.set_current(f"ecoinvent-{ei_version}-{system_model}")
 
-bw2io.import_ecoinvent_release(
-    version=ei_version,
-    system_model=system_model,
-    username=ei_user,
-    password=ei_pass,
-)
+if f"ecoinvent-{ei_version}-{system_model}" not in bw2data.databases:
+    bw2io.import_ecoinvent_release(
+        version=ei_version,
+        system_model=system_model,
+        username=ei_user,
+        password=ei_pass,
+    )
 print(f"Current project: {bw2data.projects.current}")
 print(f"Databases: {bw2data.databases}")
 
@@ -35,62 +36,66 @@ if f"ecoinvent-{ei_version}-biosphere" not in bw2data.databases:
 else:
     biosphere_name = f"ecoinvent-{ei_version}-biosphere"
 
-lci = bw2io.ExcelImporter(this_dir / "data" / "lci-hydrogen-electrolysis-ei310.xlsx")
-lci.apply_strategies()
-lci.match_database(fields=["name", "reference product", "location"])
-lci.match_database(
-    "ecoinvent-3.11-cutoff", fields=["name", "reference product", "location"]
-)
-lci.match_database(biosphere_name, fields=["name", "categories"])
-lci.statistics()
-lci.drop_unlinked(i_am_reckless=True)
-if len(list(lci.unlinked)) == 0:
-    lci.write_database()
 
-# we assign manually classifications to activities
-# since it is unclear how to do that in the Excel inventory file.
+if "h2_pem" not in bw2data.databases:
+    lci = bw2io.ExcelImporter(
+        this_dir / "data" / "lci-hydrogen-electrolysis-ei310.xlsx"
+    )
+    lci.apply_strategies()
+    lci.match_database(fields=["name", "reference product", "location"])
+    lci.match_database(
+        "ecoinvent-3.11-cutoff", fields=["name", "reference product", "location"]
+    )
+    lci.match_database(biosphere_name, fields=["name", "categories"])
+    lci.statistics()
+    lci.drop_unlinked(i_am_reckless=True)
+    if len(list(lci.unlinked)) == 0:
+        lci.write_database()
 
-classifications = {
-    "hydrogen production, gaseous, 30 bar, from PEM electrolysis, from grid electricity": (
-        "CPC",
-        "34210",
-    ),
-    "hydrogen production, gaseous, 30 bar, from PEM electrolysis, from solar photovoltaic electricity": (
-        "CPC",
-        "34210",
-    ),
-    "hydrogen production, gaseous, 30 bar, from PEM electrolysis, from onshore wind electricity": (
-        "CPC",
-        "34210",
-    ),
-    "hydrogen production, gaseous, 30 bar, from PEM electrolysis, from offshore wind electricity": (
-        "CPC",
-        "34210",
-    ),
-    "hydrogen production, gaseous, 30 bar, from PEM electrolysis, from geothermal electricity": (
-        "CPC",
-        "34210",
-    ),
-    "electrolyzer production, 1MWe, PEM, Stack": (
-        "CPC",
-        "4220:Construction of utility projects",
-    ),
-    "treatment of electrolyzer stack, 1MWe, PEM": ("CPC", "3830"),
-    "electrolyzer production, 1MWe, PEM, Balance of Plant": (
-        "CPC",
-        "4220:Construction of utility projects",
-    ),
-    "treatment of electrolyzer balance of plant, 1MWe, PEM": ("CPC", "3830"),
-    "platinum group metal, extraction and refinery operations": ("CPC", "2420"),
-    "deionized water production, via reverse osmosis, from brackish water": (
-        "CPC",
-        "34210",
-    ),
-}
-for ds in bw2data.Database("h2_pem"):
-    if ds["name"] in classifications:
-        ds["classifications"] = [classifications[ds["name"]]]
-        ds.save()
+    # we assign manually classifications to activities
+    # since it is unclear how to do that in the Excel inventory file.
+
+    classifications = {
+        "hydrogen production, gaseous, 30 bar, from PEM electrolysis, from grid electricity": (
+            "CPC",
+            "34210",
+        ),
+        "hydrogen production, gaseous, 30 bar, from PEM electrolysis, from solar photovoltaic electricity": (
+            "CPC",
+            "34210",
+        ),
+        "hydrogen production, gaseous, 30 bar, from PEM electrolysis, from onshore wind electricity": (
+            "CPC",
+            "34210",
+        ),
+        "hydrogen production, gaseous, 30 bar, from PEM electrolysis, from offshore wind electricity": (
+            "CPC",
+            "34210",
+        ),
+        "hydrogen production, gaseous, 30 bar, from PEM electrolysis, from geothermal electricity": (
+            "CPC",
+            "34210",
+        ),
+        "electrolyzer production, 1MWe, PEM, Stack": (
+            "CPC",
+            "4220:Construction of utility projects",
+        ),
+        "treatment of electrolyzer stack, 1MWe, PEM": ("CPC", "3830"),
+        "electrolyzer production, 1MWe, PEM, Balance of Plant": (
+            "CPC",
+            "4220:Construction of utility projects",
+        ),
+        "treatment of electrolyzer balance of plant, 1MWe, PEM": ("CPC", "3830"),
+        "platinum group metal, extraction and refinery operations": ("CPC", "2420"),
+        "deionized water production, via reverse osmosis, from brackish water": (
+            "CPC",
+            "34210",
+        ),
+    }
+    for ds in bw2data.Database("h2_pem"):
+        if ds["name"] in classifications:
+            ds["classifications"] = [classifications[ds["name"]]]
+            ds.save()
 
 
 def test_brightway():
